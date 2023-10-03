2b16f785374dd8a980fa8a4e3aff485517c2f6cf
Lukas Krejci <xkrejci@fi.muni.cz>
2023-10-03 22:16:24 +0200

    Commented out past methods for later inspiration. Actions kept but commented out previous use

--- src/main/java/cz/muni/fi/pv168/project/ui/model/DepartmentListModel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/DepartmentListModel.java
@@ -1,25 +1,35 @@
 package cz.muni.fi.pv168.project.ui.model;
 
-import cz.muni.fi.pv168.project.model.Department;
+
+import cz.muni.fi.pv168.project.model.Recipe;
 
 import javax.swing.AbstractListModel;
 import java.util.List;
 
-public class DepartmentListModel extends AbstractListModel<Department> {
-
-    private final List<Department> departments;
-
-    public DepartmentListModel(List<Department> departments) {
-        this.departments = departments;
-    }
-
+public class DepartmentListModel extends AbstractListModel<Recipe> {
     @Override
     public int getSize() {
-        return departments.size();
+        return 0;
     }
 
     @Override
-    public Department getElementAt(int index) {
-        return departments.get(index);
+    public Recipe getElementAt(int index) {
+        return null;
     }
+
+//    private final List<Department> departments;
+//
+//    public DepartmentListModel(List<Department> departments) {
+//        this.departments = departments;
+//    }
+//
+//    @Override
+//    public int getSize() {
+//        return departments.size();
+//    }
+//
+//    @Override
+//    public Department getElementAt(int index) {
+//        return departments.get(index);
+//    }
 }
