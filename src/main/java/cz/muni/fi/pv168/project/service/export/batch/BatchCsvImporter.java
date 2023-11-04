fa09e6b2ca2d7532ce0a546258d4c60c40b08b85
Lukas Krejci <xkrejcifi.muni.cz>
2023-11-04 20:02:16 +0100

    Added Export, Import from Employee records and rewritten to suit our app

---
+++ src/main/java/cz/muni/fi/pv168/project/service/export/batch/BatchCsvImporter.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.pv168.project.service.export.batch;
+
+import cz.muni.fi.pv168.project.service.export.format.Format;
+
+/**
+ * @author Adam Juhas
+ */
+public class BatchCsvImporter implements BatchImporter{
+    @Override
+    public Batch importBatch(String filePath) {
+        return null;
+    }
+
+    @Override
+    public Format getFormat() {
+        return null;
+    }
+}
