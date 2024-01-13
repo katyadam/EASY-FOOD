9cb04df556cd20f84e7dd41145fc08b07acafa83
Filip Škvára <xskvara@fi.muni.cz>
2024-01-13 13:16:37 +0100

    category deleteion dialog fixed

--- src/main/java/cz/muni/fi/pv168/project/business/service/validation/CategoryUsageValidator.java
+++ src/main/java/cz/muni/fi/pv168/project/business/service/validation/CategoryUsageValidator.java
@@ -1,8 +1,13 @@
 package cz.muni.fi.pv168.project.business.service.validation;
 
+import cz.muni.fi.pv168.project.business.model.AddedIngredient;
 import cz.muni.fi.pv168.project.business.model.Category;
 import cz.muni.fi.pv168.project.business.model.Recipe;
 import cz.muni.fi.pv168.project.business.repository.Repository;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CategoryUsageValidator implements Validator<Category> {
 
@@ -16,15 +21,23 @@ public class CategoryUsageValidator implements Validator<Category> {
 
     @Override
     public ValidationResult validate(Category model) {
-        int recipesWithThisModel = recipeRepository.findAll().stream()
+        List<Recipe> recipesWithThisModel = recipeRepository.findAll().stream()
                 .filter(recipe -> recipe.getCategory().getName().equals(model.getName()))
-                .toList().size();
+                .toList();
 
-        if (recipesWithThisModel > 0) {
+        if (!recipesWithThisModel.isEmpty()) {
             return ValidationResult.failed(
-                    String.format("Category: %s is used in %d recipes!", model.getName(), recipesWithThisModel)
+                    String.format("Ingredient: %s is used %d times", model, recipesWithThisModel.size()),
+                    getRecipesFormattedString(recipesWithThisModel)
             );
         }
         return ValidationResult.success();
     }
+    private String getRecipesFormattedString(List<Recipe> recipes) {
+        StringBuilder sb = new StringBuilder("In recipes:" + System.lineSeparator());
+        recipes.forEach(recipe -> sb.append("-> ").append(recipe).append(System.lineSeparator()));
+        return sb.toString();
+    }
 }
+
+
