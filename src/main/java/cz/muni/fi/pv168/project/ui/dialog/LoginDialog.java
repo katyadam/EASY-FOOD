79c80bbc75e7d444429782de6a77a09ae1088a60
Lukáš Krejčí <xkrejci@fi.muni.cz>
2024-05-17 18:24:05 +0200

    feat(login): implement better login

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/LoginDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/LoginDialog.java
@@ -1,14 +1,11 @@
 package cz.muni.fi.pv168.project.ui.dialog;
-import cz.muni.fi.pv168.project.business.model.Recipe;
 import cz.muni.fi.pv168.project.business.model.RegisteredUser;
 import cz.muni.fi.pv168.project.wiring.CommonDependencyProvider;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collections;
-import java.util.List;
 
 
 public class LoginDialog extends EntityDialog<RegisteredUser> {
@@ -17,6 +14,8 @@ public class LoginDialog extends EntityDialog<RegisteredUser> {
     private JPasswordField passwordField;
 
     private CommonDependencyProvider commonDependencyProvider;
+
+    private RegisteredUser entityUser;
 
     public LoginDialog( CommonDependencyProvider commonDependencyProvider) {
         super(new RegisteredUser("", "", "", null), Collections.emptyList());
@@ -36,10 +35,7 @@ public class LoginDialog extends EntityDialog<RegisteredUser> {
         add(loginButton);
 
         JButton registerButton = new JButton("Register"); 
-        registerButton.addActionListener(e -> {
-                RegisterDialog registerDialog = new RegisterDialog(new Frame(), commonDependencyProvider);
-                registerDialog.setVisible(true);
-        });
+        registerButton.addActionListener(commonDependencyProvider.getActionFactory().getRegisterDialogAction()::actionPerformed);
         add(registerButton);
 
     }
@@ -49,14 +45,18 @@ public class LoginDialog extends EntityDialog<RegisteredUser> {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                var exists = commonDependencyProvider.getUserRepository().existByLogin(usernameField.getText(),passwordField.getPassword().toString());
-                if (exists.isPresent()) {
-                    JOptionPane.showMessageDialog(null,"No user with this login exists.","Error",JOptionPane.ERROR_MESSAGE);
+                var username = usernameField.getText();
+                var password = new String(passwordField.getPassword());
+                // TODO: hash password
+                var user = commonDependencyProvider.getUserRepository().existByLogin(username, password);
+
+                if (!user.isPresent()) {
+                    JOptionPane.showMessageDialog(null, "Invalid username or password.", "Error", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
-                entity.setName(usernameField.getText());
-                entity.setPassword(passwordField.getPassword().toString());
-                commonDependencyProvider.getSession().setLoggedUser(entity);
+                
+                commonDependencyProvider.getSession().setLoggedUser(user.get());
+                entityUser = user.get();
             }
         });
         return loginButton;
@@ -64,6 +64,6 @@ public class LoginDialog extends EntityDialog<RegisteredUser> {
 
     @Override
     RegisteredUser getEntity() {
-        return null;
+        return entityUser;
     }
 }
